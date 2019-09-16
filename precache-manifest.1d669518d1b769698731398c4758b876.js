8432c41fb615ce342da90763cea7a6d2b4e4b878
ajinra020307 <ajinra020307@gmail.com>
2019-09-16 22:06:08 +0530

    finishedproject

---
+++ precache-manifest.1d669518d1b769698731398c4758b876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f78a71dda60333a5235572b5c3d4330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cde5c8762c137eff360",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "095c80b32b3fc3d6d629",
+    "url": "/static/js/2.cc8aca49.chunk.js"
+  },
+  {
+    "revision": "8cde5c8762c137eff360",
+    "url": "/static/js/main.ecdd74ba.chunk.js"
+  },
+  {
+    "revision": "9eaa76e77d14d57d65a4",
+    "url": "/static/js/runtime~main.b41af966.js"
+  }
+]);
